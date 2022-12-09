6d7171977493bc311fe85fe19ae8209edf992969
Simeodey <adamand12@abv.bg>
2022-12-09 19:44:21 +0200

    sda

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,15 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
-  });
+  const main = document.querySelector(".main");
+  const div = document.createElement("div",);
+  const image = document.createElement("img");
+  div.className = "image"
+  image.src="https://cdn.pixabay.com/photo/2022/11/29/16/51/sheep-7624863_640.jpg"
+  div.appendChild(image);
+  main.appendChild(div);
+  div.addEventListener("click", () =>{
+    div.style.transform = "scale(2)"
+  })
+  
 });
